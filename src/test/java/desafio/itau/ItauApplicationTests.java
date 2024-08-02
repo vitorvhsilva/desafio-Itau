813c28bc7cc037bdbe6687b4fe915659bf96a442
vitorvhsilva <dndragonbr@gmail.com>
2024-08-02 18:58:55 -0300

    Inicialização do Projeto

---
+++ src/test/java/desafio/itau/ItauApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
